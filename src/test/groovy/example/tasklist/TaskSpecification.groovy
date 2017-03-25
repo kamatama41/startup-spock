e05d0dffb9396edba06fa9f0e40703267d03dae7
Shinichi Ishimura <shiketaudonko41@gmail.com>
2017-03-25 16:15:00 +0900

    とりあえず、落ちるテストを実装

--- src/test/groovy/example/tasklist/TaskSpecification.groovy
+++ src/test/groovy/example/tasklist/TaskSpecification.groovy
@@ -11,4 +11,13 @@ class TaskSpecification extends Specification {
         then:"最初の検証"
         task.getTitle() == "タイトルです"
     }
+
+    def "期限を登録できる"(){
+        when:"最初の操作"
+        def task = new Task()
+        task.setDueTo(new Date(2017, 2, 25))
+
+        then:"最初の検証"
+        task.getDueTo() == new Date(2017, 2, 25)
+    }
 }
