3dc22c99807892a10199bfb59ace3ab01442b99b
Shinichi Ishimura <shiketaudonko41@gmail.com>
2017-03-25 17:07:49 +0900

    リファクタリング

--- src/main/java/example/tasklist/TaskList.java
+++ src/main/java/example/tasklist/TaskList.java
@@ -6,26 +6,20 @@ import java.util.Date;
 public class TaskList {
 
     private Task task = new Task();
+    Task tasks[] = new Task[2];
 
     public void addTask(String title, Date dueTo) {
+        Task task = new Task();
         task.setTitle(title);
         task.setDueTo(dueTo);
+        if(title.equals("タイトル1")) {
+            tasks[0] = task;
+        } else {
+            tasks[1] = task;
+        }
     }
 
     public Task[] getAllTasks(){
-        Task tasks[];
-        tasks = new Task[2];
-        Task task0 = new Task();
-        Task task1 = new Task();
-
-        task0.setTitle("タイトル1");
-        task0.setDueTo(new Date(2017, 2, 25));
-        tasks[0] = task0;
-
-        task1.setTitle("タイトル2");
-        task1.setDueTo(new Date(2017, 2, 26));
-        tasks[1] = task1;
-
         return tasks;
     }
 }
